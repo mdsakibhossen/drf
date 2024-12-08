07efdf5245ba9600626b34db1b9050dcfed38e01
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-12-08 10:59:34 +0600

    Serializer & Deserializer

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import students,student
+from .views import students,student,student_create
 
 urlpatterns = [
     # path("home/", view_name, name="name"),
     path("students/", students, name="students"),
     path("students/<int:pk>", student, name="student"),
+    path("create-student/", student_create, name="student_create"),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,9 @@ from .models import Student
 from .serializers import StudentSerializer
 from django.http import HttpResponse, JsonResponse
 from rest_framework.renderers import JSONRenderer
+import io
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -17,7 +20,7 @@ def students(request):
         # return HttpResponse(
         #     "Students not found", status=404, content_type="application/json"
         # )
-        return JsonResponse("Students not found", safe=False)
+        return JsonResponse({"error": "Students not found"}, status=404)
 
 
 def student(request, pk):
@@ -31,4 +34,18 @@ def student(request, pk):
         # return HttpResponse(
         #     "Student not found", status=404, content_type="application/json"
         # )
-        return JsonResponse("Student not found", safe=False)
+        return JsonResponse({"error": "Student not found"}, status=404)
+
+
+# Create Student
+@csrf_exempt
+def student_create(request):
+    if request.method == "POST":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=python_data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
