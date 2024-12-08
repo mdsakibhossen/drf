2485c342b607133eb9fab341e17763c9021cc25c
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-12-08 10:21:25 +0600

    Basic Serializer

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import students,student
 
 urlpatterns = [
     # path("home/", view_name, name="name"),
+    path("students/", students, name="students"),
+    path("students/<int:pk>", student, name="student"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from django.http import HttpResponse, JsonResponse
+from rest_framework.renderers import JSONRenderer
+
 
 # Create your views here.
+def students(request):
+    try:
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        # json_data = JSONRenderer().render(serializer.data)
+        # return HttpResponse(json_data, content_type="application/json")
+        return JsonResponse(serializer.data, safe=False)
+    except Student.DoesNotExist:
+        # return HttpResponse(
+        #     "Students not found", status=404, content_type="application/json"
+        # )
+        return JsonResponse("Students not found", safe=False)
+
+
+def student(request, pk):
+    try:
+        stu = Student.objects.get(id=pk)
+        serializer = StudentSerializer(stu)
+        # json_data = JSONRenderer().render(serializer.data)
+        # return HttpResponse(json_data, content_type="application/json")
+        return JsonResponse(serializer.data)
+    except Student.DoesNotExist:
+        # return HttpResponse(
+        #     "Student not found", status=404, content_type="application/json"
+        # )
+        return JsonResponse("Student not found", safe=False)
