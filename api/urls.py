7f8d16d697f6ae6e2846269b4e8d992e9697ca6c
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-12-10 10:54:12 +0600

    Serializer + CRUD (FBV)

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import students,student,student_create
+from .views import students
 
 urlpatterns = [
     # path("home/", view_name, name="name"),
     path("students/", students, name="students"),
-    path("students/<int:pk>", student, name="student"),
-    path("create-student/", student_create, name="student_create"),
+    path("students/<int:pk>", students, name="student"),
 ]
--- api/views.py
+++ api/views.py
@@ -9,37 +9,21 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
-def students(request):
-    try:
-        stu = Student.objects.all()
-        serializer = StudentSerializer(stu, many=True)
-        # json_data = JSONRenderer().render(serializer.data)
-        # return HttpResponse(json_data, content_type="application/json")
-        return JsonResponse(serializer.data, safe=False)
-    except Student.DoesNotExist:
-        # return HttpResponse(
-        #     "Students not found", status=404, content_type="application/json"
-        # )
-        return JsonResponse({"error": "Students not found"}, status=404)
-
-
-def student(request, pk):
-    try:
-        stu = Student.objects.get(id=pk)
-        serializer = StudentSerializer(stu)
-        # json_data = JSONRenderer().render(serializer.data)
-        # return HttpResponse(json_data, content_type="application/json")
-        return JsonResponse(serializer.data)
-    except Student.DoesNotExist:
-        # return HttpResponse(
-        #     "Student not found", status=404, content_type="application/json"
-        # )
-        return JsonResponse({"error": "Student not found"}, status=404)
-
-
-# Create Student
 @csrf_exempt
-def student_create(request):
+def students(request, pk=None):
+    if request.method == "GET":
+        try:
+            if pk == None:
+                stu = Student.objects.all()
+                serializer = StudentSerializer(stu, many=True)
+                return JsonResponse(serializer.data, safe=False)
+            else:
+                stu = Student.objects.get(id=pk)
+                serializer = StudentSerializer(stu)
+                return JsonResponse(serializer.data)
+        except Student.DoesNotExist:
+            return JsonResponse({"error": "Student Not Found"}, status=404)
+
     if request.method == "POST":
         json_data = request.body
         stream = io.BytesIO(json_data)
@@ -47,5 +31,47 @@ def student_create(request):
         serializer = StudentSerializer(data=python_data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(
+                {"message": "Student created successfully!"}, status=200
+            )
+        return JsonResponse(serializer.errors)
+
+    if request.method == "PATCH":
+        stu = Student.objects.get(id=pk)
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+
+        # Use partial=True to allow partial updates
+        serializer = StudentSerializer(instance=stu, data=python_data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(
+                {"message": "Student updated successfully!"}, status=200
+            )
         return JsonResponse(serializer.errors, status=400)
+
+    if request.method == "PUT":
+        stu = Student.objects.get(id=pk)
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+
+        # Pass both the instance and new data to the serializer
+        serializer = StudentSerializer(instance=stu, data=python_data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(
+                {"message": "Student updated successfully!"}, status=200
+            )
+        return JsonResponse(serializer.errors, status=400)
+
+    if request.method == "DELETE":
+        try:
+            stu = Student.objects.get(id=pk)
+            stu.delete()
+            return JsonResponse({"message": "Student deleted successfully"})
+        except Student.DoesNotExist:
+            return JsonResponse({"error": "Student Not Found"}, status=404)
