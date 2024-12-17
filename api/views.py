f643024ace14ebc55b42ba67c5c9c69d51b8118b
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-12-17 10:55:20 +0600

    ModelViewSets

--- api/views.py
+++ api/views.py
@@ -1,85 +1,8 @@
-from django.shortcuts import render
+from rest_framework import routers, viewsets
 from .models import Student
 from .serializers import StudentSerializer
-from django.http import HttpResponse, JsonResponse
-from rest_framework.renderers import JSONRenderer
-import io
-from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from django.views import View
 
 
-# Create your views here.
-@method_decorator(csrf_exempt, name="dispatch")
-class StudentAPI(View):
-    def get(self, request, *args, **kwargs):
-        # print("Args", args)
-        # print("KWArgs", kwargs)
-        pk = kwargs.get("pk")
-        try:
-            if pk == None:
-                stu = Student.objects.all()
-                serializer = StudentSerializer(stu, many=True)
-                return JsonResponse(serializer.data, safe=False)
-            else:
-                stu = Student.objects.get(id=pk)
-                serializer = StudentSerializer(stu)
-                return JsonResponse(serializer.data)
-        except Student.DoesNotExist:
-            return JsonResponse({"error": "Student Not Found"}, status=404)
-
-    def post(self, request, *args, **kwargs):
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        python_data = JSONParser().parse(stream)
-        serializer = StudentSerializer(data=python_data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(
-                {"message": "Student created successfully!"}, status=200
-            )
-        return JsonResponse(serializer.errors)
-
-    def patch(self, request, *args, **kwargs):
-        pk = kwargs.get("pk")
-        stu = Student.objects.get(id=pk)
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        python_data = JSONParser().parse(stream)
-
-        # Use partial=True to allow partial updates
-        serializer = StudentSerializer(instance=stu, data=python_data, partial=True)
-
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(
-                {"message": "Student updated successfully!"}, status=200
-            )
-        return JsonResponse(serializer.errors, status=400)
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get("pk")
-        stu = Student.objects.get(id=pk)
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        python_data = JSONParser().parse(stream)
-
-        # Pass both the instance and new data to the serializer
-        serializer = StudentSerializer(instance=stu, data=python_data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(
-                {"message": "Student updated successfully!"}, status=200
-            )
-        return JsonResponse(serializer.errors, status=400)
-
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get("pk")
-        try:
-            stu = Student.objects.get(id=pk)
-            stu.delete()
-            return JsonResponse({"message": "Student deleted successfully"})
-        except Student.DoesNotExist:
-            return JsonResponse({"error": "Student Not Found"}, status=404)
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
--- drf/urls.py
+++ drf/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register("students", views.StudentModelViewSet, basename="students")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
-    path("api/", include("api.urls")),
+    path("api/", include(router.urls)),
 ]
