54eb783d139f3cd1b7a73c081779b8553a8c7134
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-12-17 11:25:35 +0600

    BasicAuthenticcation & IsAuthenticated Permissions

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
-from rest_framework import routers, viewsets
+from rest_framework import viewsets
 from .models import Student
 from .serializers import StudentSerializer
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    authentication_classes = [BaseAuthentication]
+    permission_classes = [IsAuthenticated]
